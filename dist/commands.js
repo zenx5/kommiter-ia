c15d958fb86964f5a27288af010f11cab64b48ca
Octavio Martinez <omartinez1618@gmail.com>
2024-08-19 11:39:57 -0400

    Refactor: Conversión de código a ESNext y limpieza de código
    Se ha refactorizado el código para utilizar la sintaxis ESNext y se ha limpiado el código eliminando código innecesario.
    
    [by Kommiter]

--- dist/commands.js
+++ dist/commands.js
@@ -1,190 +1,104 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 import { PrismaClient } from "@prisma/client";
-import { createText, getModel } from "./ia-action";
-import { getLastChanges } from "./git-command";
-var prisma = new PrismaClient();
-export var generateCommitMessage = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var modelData, model, lastChanges, response;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, getModelAvailable()];
-            case 1:
-                modelData = _a.sent();
-                if (!modelData) {
-                    return [2 /*return*/, {
-                            code: 1,
-                            message: "Model not found"
-                        }];
-                }
-                return [4 /*yield*/, getModel(modelData)];
-            case 2:
-                model = _a.sent();
-                return [4 /*yield*/, getLastChanges()];
-            case 3:
-                lastChanges = _a.sent();
-                return [4 /*yield*/, createText(model, "genera un mensaje de commit para los siguientes cambios:\n".concat(lastChanges, ", limita la respuesta a solo describir los cambios "))];
-            case 4:
-                response = _a.sent();
-                return [2 /*return*/, {
-                        code: 0,
-                        message: response.text
-                    }];
+import { createText, getModel } from "./ia-action.js";
+import { getLastChanges } from "./git-command.js";
+const prisma = new PrismaClient();
+export const generateCommitMessage = async () => {
+    const modelData = await getModelAvailable();
+    if (!modelData) {
+        return {
+            code: 1,
+            message: "Model not found"
+        };
+    }
+    const model = await getModel(modelData);
+    const lastChanges = await getLastChanges();
+    const response = await createText(model, `genera un mensaje de commit para los siguientes cambios:\n${lastChanges}, limita la respuesta a solo describir los cambios `);
+    return {
+        code: 0,
+        message: response.text
+    };
+};
+const getModelAvailable = async () => {
+    const currentPath = process.cwd();
+    const directories = await prisma.directory.findMany();
+    const currentDirectory = directories.find(directory => directory.path === currentPath);
+    if (!currentDirectory) {
+        const [model] = await prisma.model.findMany({
+            where: {
+                isGlobal: true
+            }
+        });
+        return model;
+    }
+    return await prisma.model.findUnique({
+        where: {
+            id: currentDirectory.modelId
         }
     });
-}); };
-var getModelAvailable = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var currentPath, directories, currentDirectory, model;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                currentPath = process.cwd();
-                return [4 /*yield*/, prisma.directory.findMany()];
-            case 1:
-                directories = _a.sent();
-                currentDirectory = directories.find(function (directory) { return directory.path === currentPath; });
-                if (!!currentDirectory) return [3 /*break*/, 3];
-                return [4 /*yield*/, prisma.model.findMany({
-                        where: {
-                            isGlobal: true
-                        }
-                    })];
-            case 2:
-                model = (_a.sent())[0];
-                return [2 /*return*/, model];
-            case 3: return [4 /*yield*/, prisma.model.findUnique({
-                    where: {
-                        id: currentDirectory.modelId
-                    }
-                })];
-            case 4: return [2 /*return*/, _a.sent()];
+};
+export const createModel = async (name, provider, key, isGlobal) => {
+    const [model] = await prisma.model.findMany({
+        where: {
+            name,
+            provider,
+            key
         }
     });
-}); };
-export var createModel = function (name, provider, key, isGlobal) { return __awaiter(void 0, void 0, void 0, function () {
-    var model, newModel;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, prisma.model.findMany({
-                    where: {
-                        name: name,
-                        provider: provider,
-                        key: key
-                    }
-                })];
-            case 1:
-                model = (_a.sent())[0];
-                if (!model) return [3 /*break*/, 3];
-                if (model.isGlobal === isGlobal)
-                    return [2 /*return*/, model];
-                return [4 /*yield*/, setGlobal(model.id)];
-            case 2: return [2 /*return*/, _a.sent()];
-            case 3: return [4 /*yield*/, prisma.model.create({
-                    data: {
-                        name: name,
-                        provider: provider,
-                        key: key,
-                        isGlobal: false
-                    }
-                })];
-            case 4:
-                newModel = _a.sent();
-                if (!isGlobal) return [3 /*break*/, 6];
-                return [4 /*yield*/, setGlobal(newModel.id)];
-            case 5:
-                _a.sent();
-                _a.label = 6;
-            case 6: return [2 /*return*/, newModel];
+    if (model) {
+        if (model.isGlobal === isGlobal)
+            return model;
+        return await setGlobal(model.id);
+    }
+    const newModel = await prisma.model.create({
+        data: {
+            name,
+            provider,
+            key,
+            isGlobal: false
         }
     });
-}); };
-export var setGlobal = function (modelId) { return __awaiter(void 0, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, prisma.model.updateMany({
-                    where: {
-                        isGlobal: true
-                    },
-                    data: {
-                        isGlobal: false
-                    }
-                })];
-            case 1:
-                _a.sent();
-                return [4 /*yield*/, prisma.model.update({
-                        where: {
-                            id: modelId
-                        },
-                        data: {
-                            isGlobal: true
-                        }
-                    })];
-            case 2: return [2 /*return*/, _a.sent()];
+    if (isGlobal)
+        await setGlobal(newModel.id);
+    return newModel;
+};
+export const setGlobal = async (modelId) => {
+    await prisma.model.updateMany({
+        where: {
+            isGlobal: true
+        },
+        data: {
+            isGlobal: false
         }
     });
-}); };
-export var setPath = function (modelId, path) { return __awaiter(void 0, void 0, void 0, function () {
-    var directory;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, prisma.directory.findMany({
-                    where: {
-                        path: path
-                    }
-                })];
-            case 1:
-                directory = (_a.sent())[0];
-                if (!directory) return [3 /*break*/, 3];
-                return [4 /*yield*/, prisma.directory.update({
-                        where: {
-                            id: directory.id
-                        },
-                        data: {
-                            modelId: modelId
-                        }
-                    })];
-            case 2: return [2 /*return*/, _a.sent()];
-            case 3: return [4 /*yield*/, prisma.directory.create({
-                    data: {
-                        path: path,
-                        modelId: modelId
-                    }
-                })];
-            case 4: return [2 /*return*/, _a.sent()];
+    return await prisma.model.update({
+        where: {
+            id: modelId
+        },
+        data: {
+            isGlobal: true
         }
     });
-}); };
+};
+export const setPath = async (modelId, path) => {
+    const [directory] = await prisma.directory.findMany({
+        where: {
+            path
+        }
+    });
+    if (directory) {
+        return await prisma.directory.update({
+            where: {
+                id: directory.id
+            },
+            data: {
+                modelId
+            }
+        });
+    }
+    return await prisma.directory.create({
+        data: {
+            path,
+            modelId
+        }
+    });
+};
